446e606e616e35279267ab11abca6a199414c454
xie <2435975501@qq.com>
2020-11-10 14:01:01 +0800

    消息5

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -121,6 +121,26 @@ class WxController extends Controller
 					 $array = ['欢迎您的关注','茶花小铺欢迎您','有什么帮助您的吗?'];
                     $content = $array[array_rand($array)];
 					echo $this->Text($data,$content);
+				}elseif($data->Event=='text'){
+					$msg = $data->Content;
+					switch ($msg) {
+						case '在吗':
+							$content = "您好!有什么帮助您的吗";
+							$this->Text($data,$content);
+							break;
+						case '在':
+							$content = "您好!有什么帮助您的吗";
+							$this->Text($data,$content);
+							break;
+						case '红包':
+							$content = "想的挺美,天上有掉馅饼的好事么ლ(′◉❥◉｀ლ)";
+							$this->Text($data,$content);
+							break;
+						default:
+							$content = "欢迎您!";
+							$this->Text($data,$content);
+							break;
+					}
 				}
 
 				//自定义菜单栏
@@ -176,26 +196,7 @@ class WxController extends Controller
 						
 						$content = "错误";
 						echo $this->Text($data,$content);
-					}elseif($data->Event=='text'){
-						$msg = $data->Content;
-						switch ($msg) {
-							case '在吗':
-								$content = "您好!有什么帮助您的吗";
-								$this->Text($data,$content);
-								break;
-							case '在':
-								$content = "您好!有什么帮助您的吗";
-								$this->Text($data,$content);
-								break;
-							case '红包':
-								$content = "想的挺美,天上有掉馅饼的好事么ლ(′◉❥◉｀ლ)";
-								$this->Text($data,$content);
-								break;
-							default:
-								$content = "欢迎您!";
-								$this->Text($data,$content);
-								break;
-						}
+					}
 
 				break;
 			}

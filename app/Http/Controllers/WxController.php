0ae2443617476b5e833911839d198d91d8d9373a
xie <2435975501@qq.com>
2020-11-10 13:55:36 +0800

    消息

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -141,7 +141,29 @@ class WxController extends Controller
 					}
 				}
 
+			}elseif($data->Event=='text'){
+				$msg = $data->Content;
+				switch ($msg) {
+					case '在吗':
+						$content = "您好!有什么帮助您的吗";
+						$this->Text($data,$content);
+						break;
+					case '在':
+						$content = "您好!有什么帮助您的吗";
+						$this->Text($data,$content);
+						break;
+					case '红包':
+						$content = "想的挺美,天上有掉馅饼的好事么ლ(′◉❥◉｀ლ)";
+						$this->Text($data,$content);
+						break;
+					default:
+						$content = "欢迎您!";
+						$this->Text($data,$content);
+						break;
+				}
 			}
+
+
 			switch($data->MsgType){
 				case "text":
 					//把天气截取出来，后面是天气的地址

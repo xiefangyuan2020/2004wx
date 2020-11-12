05279da3ab6644c8ea23b919143dabcf645b9559
xie <2435975501@qq.com>
2020-11-12 21:15:26 +0800

    素材

--- app/Media.php
+++ app/Media.php
@@ -9,6 +9,6 @@ class Media extends Model
     protected $table = 'media';
     public $primaryKey = "id";
     public $timestamps = false;
-    protected $guarded=[];
-    protected $fillable = ['time'];
+    //protected $guarded=[];
+    //protected $fillable = ['time'];
 }
